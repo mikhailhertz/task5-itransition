5f9bcf79a354a9454f7368e64937b2345d9bd377
unknown <mikhailhertz@gmail.com>
2021-12-22 19:16:37 +0300

    commit

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 async function fetchMessages(setMessages) {
-    await fetch('http://localhost:9000/api/messages')
+    await fetch('https://task5-itransition.herokuapp.com/api/messages')
         .then(result => result = result.json())
         .then(result => setMessages(result))
         .catch(error => console.log(error));
